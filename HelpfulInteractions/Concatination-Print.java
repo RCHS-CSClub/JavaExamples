2df1b00b58020ca02b18dcb3d635026ac642f223
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2023-02-03 08:53:45 -0800

    Create Concatination-Print.java

---
+++ HelpfulInteractions/Concatination-Print.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        String myStr = "I like ";
+        String myStr2 = "I hate ";
+        myStr += "turtles " + 1 + 2;
+        
+        System.out.println(myStr);
+        System.out.println(myStr2 + "flies " + 1 + 2);
+    }
+  }
